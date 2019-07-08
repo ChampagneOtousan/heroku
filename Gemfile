9147449fd4b8aab696ad2d78218fbc642b7a7316
Esteban Cuesta <cuestaesteban@yahoo.com>
2019-07-08 14:34:33 -0400

    aye

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source "https://rubygems.org" do
+
+  gem "sinatra"
+  gem "sinatra-activerecord"
+  gem "sinatra-flash"
+  gem "sqlite3"
+  gem "rake"
+  gem "httparty"
+
+end
--- server.rb
+++ server.rb
@@ -2,6 +2,29 @@ require "sinatra"
 require "sinatra/activerecord"
 require "sinatra/flash"
 require "sqlite3"
-require "bootstrap"
+require "rake"
+require "httparty"
 
 enable :sessions
+
+user = User.new
+
+get "/signup" do
+  erb :signup
+end
+
+get "/" do
+  erb :home
+end
+
+get "/profile" do
+  erb :profile
+end
+
+get "/posts" do
+  erb :posts
+end
+
+get "/settings/account" do
+  erb :"settings/account"
+end
